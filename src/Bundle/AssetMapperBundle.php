1d5b54cf2e309f53d0342d7543311e8aab5c225c
tacman <tacman@users.noreply.github.com>
2026-03-30 11:40:08 +0000

    refactor bundles that has Assets to use AssetMapperBundle

--- src/Bundle/AssetMapperBundle.php
+++ src/Bundle/AssetMapperBundle.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace Survos\CoreBundle\Traits;
+declare(strict_types=1);
 
+namespace Survos\CoreBundle\Bundle;
+
+use Survos\CoreBundle\HasAssetMapperInterface;
 use Symfony\Component\AssetMapper\AssetMapperInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-trait HasAssetMapperTrait
+abstract class AssetMapperBundle extends AbstractBundle implements HasAssetMapperInterface
 {
-    // Backward compatibility for bundles not yet migrated to AssetMapperBundle.
-    // Preferred in bundle class: const ASSET_PACKAGE = 'foo';
-    // Optional override: const ASSET_NAMESPACE = '@survos/foo';
-    // Also ensure "symfony-ux" is in composer.json keywords for auto-registration.
-
+    // IMPORTANT: bundle composer.json must include the "symfony-ux" keyword,
+    // otherwise Symfony UX won't auto-discover controllers from assets/package.json.
     public function isAssetMapperAvailable(ContainerBuilder $container): bool
     {
         return interface_exists(AssetMapperInterface::class)
@@ -21,11 +22,8 @@ trait HasAssetMapperTrait
 
     public function getPaths(): array
     {
-        $bundlePath = method_exists($this, 'getPath')
-            ? $this->getPath()
-            : dirname((new \ReflectionClass($this))->getFileName(), 2);
-        $dir = realpath($bundlePath.'/assets');
-        assert($dir && file_exists($dir), 'assets path must exist: '.$bundlePath);
+        $dir = realpath($this->getPath().'/assets');
+        assert($dir && file_exists($dir), 'assets path must exist: '.$this->getPath());
 
         return [$dir => $this->getAssetNamespace()];
     }
@@ -49,7 +47,7 @@ trait HasAssetMapperTrait
 
             $package = preg_replace('#^survos/#', '', $package) ?? $package;
 
-            return '@survos/' . trim($package, '/');
+            return '@survos/'.trim($package, '/');
         }
 
         $shortName = (new \ReflectionClass($this))->getShortName();
@@ -57,7 +55,7 @@ trait HasAssetMapperTrait
         $shortName = preg_replace('/Bundle$/', '', $shortName) ?? $shortName;
         $slug = strtolower((string) preg_replace('/(?<!^)[A-Z]/', '-$0', $shortName));
 
-        return '@survos/' . $slug;
+        return '@survos/'.$slug;
     }
 
     public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
