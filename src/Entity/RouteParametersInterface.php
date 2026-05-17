dde5489dcd3e1d52bb76d31019886266426e1bcb
tacman <tacman@users.noreply.github.com>
2026-05-17 16:33:37 +0000

    tui-extras, folio, deprecated pixie

--- src/Entity/RouteParametersInterface.php
+++ src/Entity/RouteParametersInterface.php
@@ -2,6 +2,11 @@
 
 namespace Survos\CoreBundle\Entity;
 
+/**
+ * @deprecated Use #[Survos\FieldBundle\Attribute\RouteIdentity] + Survos\FieldBundle\Entity\RouteIdentityTrait instead.
+ *             The interface contract (getRp, getUniqueIdentifiers, getClassnamePrefix) is fulfilled by RouteIdentityTrait.
+ *             Keep `implements RouteParametersInterface` on existing entities until field-bundle fully replaces this interface.
+ */
 interface RouteParametersInterface
 {
     public function getUniqueIdentifiers(): array;
